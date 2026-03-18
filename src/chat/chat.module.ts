5c26e0c7b16dfe9eb54d02ad0412bf40433d46ec
Jared-Farfan <Jaredfarfan06@gmail.com>
2026-03-17 21:40:24 -0500

    postinstall

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,11 +1,13 @@
+
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { AuthModule } from '../auth/auth.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RedisModule],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
