563fb16a617a31a95973bcba0c79a53b78729cd3
Jared-Farfan <Jaredfarfan06@gmail.com>
2026-03-17 20:11:21 -0500

    documentacion swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*', methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'] });
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Traffic Simulator API')
+    .setDescription('Documentación de la API del backend')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Backend running on port ${port}`);
