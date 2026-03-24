a990a71878881e2dd0023896a51d0eb02fd248ed
Jared-Farfan <Jaredfarfan06@gmail.com>
2026-03-23 20:48:17 -0500

    Guards de usuarios

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -6,13 +6,13 @@ import { RolesGuard } from 'src/users/roles.guard';
 import { Role } from '@prisma/client';
 
 @Controller('api/chat')
-// @UseGuards(AuthGuard)
+@UseGuards(AuthGuard)
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Get('messages')
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN , Role.USER)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN , Role.USER)
   getMessages(@Query('limit') limit: string, @Query('cursor') cursor?: string) {
     return this.chatService.getMessages(parseInt(limit || '50', 10), cursor);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,41 +15,41 @@ import { RolesGuard } from './roles.guard';
 import { Roles } from './roles.decorator';
 
 @Controller('api/users')
-// @UseGuards(AuthGuard)
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN, Role.USER)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN, Role.USER)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Patch(':id/role')
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
   updateRole(@Param('id') id: string, @Body() body: { role: Role }) {
     return this.usersService.updateRole(id, body.role);
   }
 
   @Delete(':id')
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
   }
 
   @Get('allActive')
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN, Role.USER)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN, Role.USER)
   findAllActive() {
     return this.usersService.findAllActive();
   }
 
   @Patch(':id/estatus')
-  // @UseGuards(RolesGuard)
-  // @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
   changeEstatus(@Param('id') id: string, @Body() body: { estatus: Estatus }) {
     return this.usersService.changeEstatus(id, body.estatus);
   }
