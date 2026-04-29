a83b3cbf1ace82650510a2585bb4e0578842aedd
Jared-Farfan <Jaredfarfan06@gmail.com>
2026-04-28 23:49:31 -0500

    modulo de historial eliminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ChatModule } from './chat/chat.module';
-import { HistoryModule } from './history/history.module';
 import { RedisModule } from './redis/redis.module';
-import { AzureModule } from './azure/azure.module';
 import { HealthController } from './health/health.controller';
 
 @Module({
@@ -20,10 +17,7 @@ import { HealthController } from './health/health.controller';
     PrismaModule,
     AuthModule,
     UsersModule,
-    ChatModule,
-    HistoryModule,
     RedisModule,
-    AzureModule,
   ],
   controllers: [HealthController],
 })
